f160975fa74721abf0199581b54a5eb96dbf7c7b
Peter Malík <p.malik@malikp.eu>
2019-06-02 23:07:24 +0200

    Add attributes

---
+++ TwitchLib.Client.Enums/Annotation/IntervalAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TwitchLib.Client.Enums.Annotation
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class IntervalAttribute : Attribute
+    {
+        public IntervalAttribute(int from, int to)
+        {
+            To = to;
+            From = from;
+        }
+
+        public int To { get; }
+
+        public int From { get; }
+    }
+}
---
+++ TwitchLib.Client.Enums/Annotation/MapAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwitchLib.Client.Enums.Annotation
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class MapAttribute : Attribute
+    {
+        public MapAttribute(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+}
