fac5b7b0eca568f3c7fb8770e14d68b3fa337c7d
Peter Malík <p.malik@malikp.eu>
2019-06-02 22:28:06 +0200

    Simplify Commercial extension. This could prevent defects

--- TwitchLib.Client/Extensions/CommercialExt.cs
+++ TwitchLib.Client/Extensions/CommercialExt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using TwitchLib.Client.Interfaces;
 using TwitchLib.Client.Models;
 
@@ -18,29 +19,7 @@ namespace TwitchLib.Client.Extensions
         /// <exception cref="ArgumentOutOfRangeException">length - null</exception>
         public static void StartCommercial(this ITwitchClient client, JoinedChannel channel, Enums.CommercialLength length)
         {
-            switch (length)
-            {
-                case Enums.CommercialLength.Seconds30:
-                    client.SendMessage(channel, ".commercial 30");
-                    break;
-                case Enums.CommercialLength.Seconds60:
-                    client.SendMessage(channel, ".commercial 60");
-                    break;
-                case Enums.CommercialLength.Seconds90:
-                    client.SendMessage(channel, ".commercial 90");
-                    break;
-                case Enums.CommercialLength.Seconds120:
-                    client.SendMessage(channel, ".commercial 120");
-                    break;
-                case Enums.CommercialLength.Seconds150:
-                    client.SendMessage(channel, ".commercial 150");
-                    break;
-                case Enums.CommercialLength.Seconds180:
-                    client.SendMessage(channel, ".commercial 180");
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(length), length, null);
-            }
+            client.SendMessage(channel, $".commercial {(int)length}");
         }
 
         /// <summary>
@@ -52,29 +31,7 @@ namespace TwitchLib.Client.Extensions
         /// <exception cref="ArgumentOutOfRangeException">length - null</exception>
         public static void StartCommercial(this ITwitchClient client, string channel, Enums.CommercialLength length)
         {
-            switch (length)
-            {
-                case Enums.CommercialLength.Seconds30:
-                    client.SendMessage(channel, ".commercial 30");
-                    break;
-                case Enums.CommercialLength.Seconds60:
-                    client.SendMessage(channel, ".commercial 60");
-                    break;
-                case Enums.CommercialLength.Seconds90:
-                    client.SendMessage(channel, ".commercial 90");
-                    break;
-                case Enums.CommercialLength.Seconds120:
-                    client.SendMessage(channel, ".commercial 120");
-                    break;
-                case Enums.CommercialLength.Seconds150:
-                    client.SendMessage(channel, ".commercial 150");
-                    break;
-                case Enums.CommercialLength.Seconds180:
-                    client.SendMessage(channel, ".commercial 180");
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(length), length, null);
-            }
+            StartCommercial(client, new JoinedChannel(channel), length);
         }
     }
 }
