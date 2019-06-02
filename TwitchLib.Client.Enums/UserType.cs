747d77ce61e8794284454bb19b396ff366c38c5b
Peter Malík <p.malik@malikp.eu>
2019-06-02 23:09:55 +0200

    Add mapings

--- TwitchLib.Client.Enums/UserType.cs
+++ TwitchLib.Client.Enums/UserType.cs
@@ -1,19 +1,30 @@
-﻿namespace TwitchLib.Client.Enums
+﻿using TwitchLib.Client.Enums.Annotation;
+
+namespace TwitchLib.Client.Enums
 {
     /// <summary>Enum representing various user-types.</summary>
     public enum UserType : byte
     {
         /// <summary>The standard user-type representing a standard viewer.</summary>
         Viewer,
+
         /// <summary>User-type representing viewers with channel-specific moderation powers.</summary>
+        [Map("mod")]
         Moderator,
+
         /// <summary>User-type representing viewers with Twitch-wide moderation powers.</summary>
+        [Map("global_mod")]
         GlobalModerator,
+
         /// <summary>User-type representing the broadcaster of the channel</summary>
         Broadcaster,
+
         /// <summary>User-type representing viewers with Twitch-wide moderation powers that are paid.</summary>
+        [Map("admin")]
         Admin,
+
         /// <summary>User-type representing viewers that are Twitch employees.</summary>
+        [Map("staff")]
         Staff
     }
 }
